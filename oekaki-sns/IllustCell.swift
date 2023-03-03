58b86e6a6816cd9dace8b61e23e4c8fe932b4c71
thaitani <42672817+thaitani@users.noreply.github.com>
2023-03-03 17:11:52 +0900

    feat: グリッドビューのところまで

---
+++ oekaki-sns/IllustCell.swift
@@ -0,0 +1,15 @@
+//
+//  IllustCell.swift
+//  oekaki-sns
+//
+//  Created by TOSHIKI HAITANI on 2023/03/03.
+//
+
+import UIKit
+
+class IllustCell: UICollectionViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = .red
+    }
+}
